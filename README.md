012771d7c39b24bdc402be68702c1bbb53c37a8e
slawtul <slawtul@javablues.org>
2021-01-23 18:15:01 +0100

    Add: license to every file

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Hamster
-
 ---
+
 Clipboard manager for elementary OS linux distro
 
-### Development
+## Development
 
 Before running app on you local machine, make sure you have *libgtkmm* lib installed
 
 ```bash
 sudo apt install libgtkmm-3.0-dev libgtkmm-3.0-doc
 ```
-
-
--- hamster_lib/stage.h
+++ hamster_lib/stage.h
@@ -1,11 +1,29 @@
+/*
+ * Copyright (c) 2021 Slawek Tuleja
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Slawek Tuleja <slawek.tuleja@gmail.com>
+ */
+
 #ifndef STAGE_H
 #define STAGE_H
 
 #include <iostream>
 #include <gtkmm-3.0/gtkmm.h>
 
-
-struct Stage: public Gtk::Box
+struct Stage : public Gtk::Box
 {
     Stage();
     ~Stage() override;
@@ -21,6 +39,5 @@ struct Stage: public Gtk::Box
     bool on_focus(Gtk::DirectionType direction);
     void on_search_change();
 };
-
 
 #endif //STAGE_H
