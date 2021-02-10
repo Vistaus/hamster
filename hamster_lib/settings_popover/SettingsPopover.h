cf137c8fbb0bda184db4df30efe454e1ac88dfd4
slawtul <slawtul@javablues.org>
2021-02-10 22:49:56 +0100

    Refactoring...

--- hamster_lib/settings_popover/SettingsPopover.h
+++ hamster_lib/settings_popover/SettingsPopover.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef HAMSTER_SETTINGSPOPOVER_H
 #define HAMSTER_SETTINGSPOPOVER_H
 
