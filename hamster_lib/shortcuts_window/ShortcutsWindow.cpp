cb71c0fa5423863ee1c0f1b060bb6724c36c8091
slawtul <slawtul@javablues.org>
2021-02-12 15:20:26 +0100

    Improve: change shortcut for item prefix/suffix feature

--- hamster_lib/shortcuts_window/ShortcutsWindow.cpp
+++ hamster_lib/shortcuts_window/ShortcutsWindow.cpp
@@ -60,7 +60,7 @@ ShortcutsWindow::ShortcutsWindow()
 
     item_prefix_suffix_shortcut.property_title().set_value(_("Set item prefix or suffix before pasting"));
     item_prefix_suffix_shortcut.property_shortcut_type().set_value(Gtk::SHORTCUT_ACCELERATOR);
-    item_prefix_suffix_shortcut.property_accelerator().set_value("G");
+    item_prefix_suffix_shortcut.property_accelerator().set_value("<Shift>Return");
 
     letter_case_shortcut.property_title().set_value(_("Change letter case"));
     letter_case_shortcut.property_shortcut_type().set_value(Gtk::SHORTCUT_ACCELERATOR);
