1589059ffce6cc1f8d97aee053184667ae172b8f
slawtul <slawtul@javablues.org>
2021-02-13 19:16:13 +0100

    Add: user can now see hamster logo in 'About' dialog

--- hamster_lib/about_dialog/AboutDialog.cpp
+++ hamster_lib/about_dialog/AboutDialog.cpp
@@ -26,5 +26,5 @@ AboutDialog::AboutDialog()
     set_license_type(Gtk::LICENSE_GPL_3_0);
     set_website("https://github.com/slawtul/Hamster");
     set_website_label("https://github.com/slawtul/Hamster");
+    set_logo(Gdk::Pixbuf::create_from_file("./data/icons/128/com.github.slawtul.hamster.svg"));
 }
-
