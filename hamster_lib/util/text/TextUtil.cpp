a5497df1097a73e0125792111ba2e3f9808035a8
slawtul <slawtul@javablues.org>
2021-02-27 00:23:58 +0100

    Working on text utils...

--- hamster_lib/util/text/TextUtil.cpp
+++ hamster_lib/util/text/TextUtil.cpp
@@ -16,13 +16,35 @@
  */
 #include "TextUtil.h"
 
-Glib::ustring TextUtil::short_text(Glib::ustring text, uint n_letters)
+Glib::ustring TextUtil::sub_str(const Glib::ustring &text, uint n_letters, const Glib::ustring &end)
 {
-    if (text.size() > n_letters)
+    if (text.length() > n_letters)
     {
-        return text.substr(0, n_letters).append("...");
-    } else
-    {
-        return text;
+        return text.substr(0, n_letters).append(end);
     }
+    return text;
+}
+
+bool TextUtil::has_only_spaces(const Glib::ustring &text)
+{
+    return text.find_first_not_of(" \t\n\v\f\r") == Glib::ustring::npos;
+}
+
+Glib::ustring TextUtil::join_lines(Glib::ustring &text, uint n_letters)
+{
+    const auto re = std::regex{"\\s{1,}"};
+    return std::regex_replace(text.substr(0, n_letters).c_str(), re, " ");
+}
+
+Glib::ustring TextUtil::trim_str(const Glib::ustring &text)
+{
+    const auto whitespaces = " \t";
+    const auto begin = text.find_first_not_of(whitespaces);
+    if (begin == std::string::npos)
+    {
+        return ""; // No content
+    }
+    const auto end = text.find_last_not_of(whitespaces);
+    const auto range = end - begin + 1;
+    return text.substr(begin, range);
 }
